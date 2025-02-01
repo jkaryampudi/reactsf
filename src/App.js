87ad48ec09390d48b389886d5efe8a0413ce44d5
Jay Karyampudi <71486788+jkaryampudi@users.noreply.github.com>
2025-02-01 11:50:35 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import MainComponent from "./MainComponent"; // Ensure this file exists in your src folder
 
 const AZURE_AD_DOMAIN = "https://login.microsoftonline.com/d3ad2e7f-49fe-4b21-88e1-58b4700e6a3b/v2.0"; 
-const AZURE_AD_CLIENT_ID = "a1a14eb0-8783-4bf5-aad8-ce0f074e051b"; 
+const AZURE_AD_CLIENT_ID = "38a5eebe-5693-4fa1-937b-4e30fe05ea0d"; 
 const REDIRECT_URI = "https://lemon-beach-00530c70f.4.azurestaticapps.net"; // Your Azure Static Web App URL
 
 const App = () => {
